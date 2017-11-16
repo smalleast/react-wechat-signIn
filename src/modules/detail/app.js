7e9b11f1582c580660bb43fcd4a244433bd3cec8
zhulogic <38202618@qq.com>
2017-11-16 10:50:26 +0800

    初步功能已完成

--- src/modules/detail/app.js
+++ src/modules/detail/app.js
@@ -1,6 +1,6 @@
-import AppBase, {$api, $app, $store, $utils,$wechat} from 'components/scripts/index';
-
-
+import AppBase, {$api, $app, $store, $utils, $wechat} from 'components/scripts/index';
+import classNames from 'classnames';
+import {Modal} from 'antd-mobile';
 
 export default class extends AppBase {
 
@@ -10,9 +10,17 @@ export default class extends AppBase {
         wxImages: []
       },
       memory: {
-        imgList: [],
-        detail: {},
-        changeData: []
+        list: [{
+          active: false
+        }, {
+          active: true
+        }, {
+          active: false
+        }, {
+          active: false
+        }, {
+          active: true
+        }]
       }
     }
   }
@@ -22,43 +30,59 @@ export default class extends AppBase {
   }
 
   init() {
-    this._products().then(res => {
-      let specs = res.data.specs;
-      AppBase.$.memory = {
-        detail: res.data,
-        changeData: specs
-      };
 
-      this._swiperList(res.data);
-    })
   }
 
-  _products() {
-    const {id} = nx.hashlize();
-    return $api.products('get',id,'share');
-  }
-
-  _swiperList(inData) {
-    let list = [];
-    inData.partFiles && inData.partFiles.map((item, index) => {
-      list.push(item.key);
-    });
-
-    AppBase.$.memory = {imgList: list}
-  }
+  signClick = () => {
+    const alertInstance = Modal.alert('确认签到', '您确认要签到吗？签到后是不能取消的哦，请谨慎操作哦', [
+      {
+        text: '取消', onPress: () => {
+        console.log('取消');
+      }
+      },
+      {
+        text: '确认', onPress: () => {
+        console.log('确认');
+      }
+      }
+    ])
+  };
 
   componentDidMount() {
-    $utils.downloadLink();
-    const {id} = nx.hashlize();
-    $wechat.settingShare(id);
 
   }
 
   render() {
-    const {detail, imgList, changeData} = AppBase.$.memory;
+    const {list} = AppBase.$.memory;
     return (
-      <div className="product-detail">
+      <div className="detail">
+        <div className="hd bg-47">
+          <div className="content tc">
+            <output className="f16 c-f db">舞蹈课001</output>
+            <output className="f10 c-f db mt10">费先生</output>
+            <output className="f10 c-f db mt5">11月12号-09:23-10:20</output>
+          </div>
+        </div>
+        <div className="bd">
+          {
+            list.map((item, index) => {
+              return ( <div key={index} className="list">
+                <div className="row row-center">
+                  <div className="col left pl10 tl">
+                    <output className="db c-50">费先生</output>
+                    <output className="db c-137">18520953265</output>
+                  </div>
+                  <div className="col right pr10 tr">
+                    <button onClick={this.signClick.bind(this)}
+                            className={classNames('button', {'active': item.active})}>签到
+                    </button>
+                  </div>
+                </div>
+              </div>)
+            })
+          }
 
+        </div>
       </div>
     )
   }
--- src/modules/list/app.js
+++ src/modules/list/app.js
@@ -9,9 +9,7 @@ export default class extends AppBase {
         wxImages: []
       },
       memory: {
-        imgList: [],
-        detail: {},
-        changeData: []
+        list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
       }
     }
   }
@@ -31,29 +29,37 @@ export default class extends AppBase {
   }
 
   render() {
-    const {detail, imgList, changeData} = AppBase.$.memory;
+    const {list} = AppBase.$.memory;
     return (
       <div className="list">
         <div className="row row-center wp-auto hd">
-          <div className="col tl">费先生</div>
-          <div className="col tr">09:23-10:20</div>
+          <div className="col tl c-50">费先生</div>
+          <div className="col tr c-137">09:23-10:20</div>
         </div>
         <div className="line-d-1"/>
-        <div className="row row-center wp-auto bd">
-          <div className="left tc">
-            <RA size=".76rem" url="http://oss.zhulogic.com/product_image/cp7b4b4b7c12e64b9b97ee06553eb3af90.png?x-oss-process=style/w660"/>
-          </div>
-          <div className="center tl">
-            <output className="db">舞蹈课001</output>
-            <output>1课次</output>
-          </div>
-          <div className="col right tr">
-            <div className="sign">
-              <output className="db">扫码签到</output>
-              <output>3人预约</output>
-            </div>
-          </div>
-        </div>
+        {
+          list.map((item, index) => {
+            return ( <div key={index} className="row row-center wp-auto bd">
+              <div className="left tc">
+                <div className="left-avatar">
+                  <RA size=".76rem" radius=".1rem"
+                      url="http://oss.zhulogic.com/product_image/cp7b4b4b7c12e64b9b97ee06553eb3af90.png?x-oss-process=style/w660"/>
+                </div>
+              </div>
+              <div className="center tl">
+                <output className="db c-50">舞蹈课001</output>
+                <output className="c-137">1课次</output>
+              </div>
+              <div className="col right tr">
+                <div className="sign tc">
+                  <output className="db f12">扫码签到</output>
+                  <output className="f10">3人预约</output>
+                </div>
+              </div>
+            </div>)
+          })
+        }
+
       </div>
     )
   }
